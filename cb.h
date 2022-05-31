900b51efcae8c8cfcd6851d1969fa520df6b682d
baboomerang <irodrigue101@gmail.com>
2022-05-30 23:24:28 -0400

    implemented all of the data structure (UNTESTED)

--- cb.h
+++ cb.h
@@ -1,7 +1,8 @@
-#include <iterator>
-#include <utility>
-#include <limits>
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
 #include <stdexcept>
+#include <limits>
 
   /**
    *  @brief A fixed size container connected end to end
@@ -13,10 +14,10 @@
    *  Sets support random access iterators.
    *
    *  @tparam  T  Type of element. Required to be a complete type.
-   *  @tparam  S  Size of the container (number of elements)
+   *  @tparam  N  Number of elements in the container.
   */
 
-template<typename T, std::size_t S>
+template<typename T, std::size_t N>
 class circular_buffer {
 public:
     using value_type             = T;
@@ -34,138 +35,173 @@ public:
         using pointer            = value_type*;
         using reference          = value_type&;
 
-        explicit iterator(pointer ptr) : m_pos(ptr - array_), m_ptr(ptr) {
-            if (m_pos < 0 || m_pos >= array_size_) {
-                throw std::out_of_range(std::string("circular_buffer::circular_buffer: iterator position " + m_pos)
-                        + std::string(" is < 0  or >= container size: " + array_size_));
+        explicit iterator(pointer ptr, size_type pos) : m_ptr(ptr), m_pos(pos) {}
+        ~iterator() = default;
+
+        iterator& operator=(const iterator& rhs) {
+            if (this == &rhs) {
+                return *this;
             }
+            m_ptr = rhs.m_ptr;
+            m_pos = rhs.m_pos;
+            return *this;
         }
-        //iterator(const iterator&);
-        ~iterator() { delete[] m_ptr; }
+        bool operator==(const iterator& rhs) const { return (m_pos == rhs.m_pos) && (m_ptr == rhs.m_ptr); }
+        bool operator!=(const iterator& rhs) const { return (m_pos != rhs.m_pos) || (m_ptr != rhs.m_ptr); }
 
-        iterator& operator=(const iterator& rhs) { return rhs; }
-        friend bool operator==(const iterator& lhs, const iterator& rhs) { return lhs.m_ptr == rhs.m_ptr; }
-        friend bool operator!=(const iterator& lhs, const iterator& rhs) { return lhs.m_ptr != rhs.m_ptr; }
-        //bool operator< (const iterator& a, const iterator& b) const { return a.m_ptr <  b.m_ptr; }
-        //bool operator> (const iterator& a, const iterator& b) const { return a.m_ptr >  b.m_ptr; }
-        //bool operator<=(const iterator& a, const iterator& b) const { return a.m_ptr <= b.m_ptr; }
-        //bool operator>=(const iterator& a, const iterator& b) const { return a.m_ptr >= b.m_ptr; }
+        // Dereferencing operators
+        reference operator*() const { return m_ptr[m_pos]; }
+        pointer operator->() { return m_ptr + m_pos; }
 
-        // TODO: change the logic of these operators to allow iterators to 'wrap' over the container
-        iterator& operator++() { ++m_ptr; return *this; }
-        iterator& operator--() { --m_ptr; return *this; }
-        iterator operator++(int) { iterator tmp = *this; ++m_ptr; return tmp; }
-        iterator operator--(int) { iterator tmp = *this; --m_ptr; return tmp; }
+        // Prefix addition and subtraction operators
+        iterator& operator++() {
+            ++m_pos;
+            if (m_pos == array_size_) {
+                m_pos = 0;
+            }
+            return *this;
+        }
+        iterator& operator--() {
+            if (m_pos == 0) {
+                m_pos = array_size_ - 1;
+            } else {
+                --m_pos;
+            }
+            return *this;
+        }
 
-        iterator& operator+=(size_type);
-        iterator& operator-=(size_type);
-        iterator operator+(size_type) const;
-        iterator operator-(size_type) const;
+        // Postfix addition and subtraction operators
+        iterator operator++(int) {
+            iterator tmp = *this;
+            operator++();
+            return tmp;
+        }
+        iterator operator--(int) {
+            iterator tmp = *this;
+            operator--();
+            return tmp;
+        }
 
-        iterator operator+(size_type, const iterator&);
-        difference_type operator-(iterator) const;
+        // Compound addition and subtraction operators
+        iterator& operator+=(size_type length) {
+            m_pos = add_wrap(m_pos, length);
+            return *this;
+        }
+        iterator& operator-=(size_type length) {
+            size_type new_pos;
+            m_pos = subtract_wrap(m_pos, length);
+            return *this;
+        }
 
-        reference operator[](size_type) const { return; }
-        reference operator*() const;
-        pointer operator->() const;
+        // Binary addition and subtraction operators
+        iterator operator+(size_type length) const {
+            iterator tmp = *this;
+            tmp.m_pos = add_wrap(tmp.m_pos, length);
+            return tmp;
+        }
+        iterator operator-(size_type length) const {
+            iterator tmp = *this;
+            tmp.m_pos = subtract_wrap(tmp.m_pos, length);
+            return tmp;
+        }
 
-        private:
-            difference_type m_pos;
-            pointer m_ptr;
+    private:
+        size_type subtract_wrap(size_type pos, size_type length) {
+            size_type new_pos;
+            if (length > pos) {
+                size_type remainder = (length - pos) % array_size_;
+                new_pos = array_size_ - remainder;
+            } else {
+                new_pos = pos - length;
+            }
+            return new_pos;
+        }
+        size_type add_wrap(size_type pos, size_type length) {
+            size_type new_pos = pos + length;
+            if (new_pos >= array_size_) {
+                new_pos %= array_size_;
+            }
+            return new_pos;
+        }
+        pointer m_ptr;
+        size_type m_pos;
     };
+    //===========================================================================
 
-    //using reverse_iterator       = std::reverse_iterator<iterator>;
-    //using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    iterator begin() noexcept {
+        return iterator(array_, start_);
+    }
 
-    // Iterators.
-    iterator begin() {};
-    //const_iterator begin() const;
-    //const_iterator cbegin() const;
+    // end() returns an iterator which is the past-the-end value for the container
+    iterator end() noexcept {
+        size_type true_end = end_ + 1;
+        return iterator(array_, (true_end != array_size_) ? true_end : 0);
+    }
 
-    iterator end();
-    //const_iterator end() const;
-    //const_iterator cend() const;
-
-    //reverse_iterator rbegin();
-    //const_reverse_iterator rbegin() const;
-    //const_reverse_iterator crbegin() const;
-
-    //reverse_iterator rend();
-    //const_reverse_iterator rend() const;
-    //const_reverse_iterator crend() const;
-
-    // Class Methods.
-    explicit circular_buffer(value_type, size_type capacity = 0) :
-        array_(new value_type[capacity]),
-        array_size_(capacity),
+    circular_buffer() :
+        array_{0},
+        array_size_(N),
         start_(0),
         end_(0),
-        contents_size_(0) {}
+        contents_size_(0){}
 
-    ~circular_buffer() { delete[] array_; };
+    ~circular_buffer(){}
 
     void push_back(const_reference item) {
-        inc_end();
-        if (contents_size_ == array_size_) {
-            inc_start();
-        }
         array_[end_] = item;
+        increment_end();
+        if (contents_size_ == array_size_) {
+            increment_start();
+        }
     }
 
     void pop_front() {
-        inc_start();
+        increment_start();
     }
 
-    // Capacity.
+    // Capacity
     constexpr size_type size() const noexcept { return contents_size_; }
     constexpr size_type capacity() const noexcept { return array_size_; }
     constexpr size_type max_size() const noexcept { return std::numeric_limits<size_type>::max(); }
     constexpr bool empty() const noexcept { return contents_size_ == 0; }
     void clear() noexcept { start_ = end_ = contents_size_ = 0; }
 
-    // Element access.
+    // Element access
     reference operator[](size_type pos) noexcept { return array_[pos]; }
-    constexpr const_reference operator[](size_type pos) const noexcept { return const_cast<reference>(array_[pos]); }
+    constexpr const_reference operator[](size_type pos) const noexcept { return array_[pos]; }
     reference at(size_type pos) {
         if (pos >= array_size_) {
-            throw std::out_of_range(std::string("circular_buffer::at: " + pos) + std::string(" is >= " + array_size_));
+            std::out_of_range(std::string("circular_buffer::at index: " + pos) +
+                              std::string(" is >= array size: " + array_size_));
         }
         return array_[pos];
     }
     constexpr const_reference at(size_type pos) const {
-        return (pos < array_size_) ? const_cast<reference>(array_[pos])
-            : throw std::out_of_range(std::string("circular_buffer::at: " + pos) + std::string(" is >= " + array_size_));
+        return (pos >= array_size_) ? array_[pos]
+            : throw std::out_of_range(std::string("circular_buffer::at index: " + pos) +
+                                      std::string(" is >= array size: " + array_size_));
     }
     reference front() noexcept { return array_[start_]; }
     reference back() noexcept { return array_[end_]; }
-    constexpr const_reference front() const noexcept { return const_cast<reference>(array_[start_]); }
-    constexpr const_reference back() const noexcept { return const_cast<reference>(array_[end_]); }
+    constexpr const_reference front() const noexcept { return array_[start_]; }
+    constexpr const_reference back() const noexcept { return array_[end_]; }
     pointer data() noexcept { return array_; }
-    const_pointer data() const noexcept { return const_cast<pointer>(array_); }
+    const_pointer data() const noexcept { return array_; }
 
 private:
-    pointer array_;
+    void increment_wrap(size_type& value) {
+        ++value;
+        if (value == array_size_) {
+            value = 0;
+        }
+    }
+    void increment_end() { increment_wrap(end_); ++contents_size_; }
+    void increment_start() { increment_wrap(start_); --contents_size_; }
+    value_type array_[N];
     size_type array_size_;
     size_type start_;
     size_type end_;
     size_type contents_size_;
-
-    void inc_start() {    // head
-        if (empty) {
-            return;
-        }
-        ++start_;
-        --contents_size_;
-        if (start_ == array_size_) {
-            start_ = 0;
-        }
-    }
-
-    void inc_end() {     // tail
-        ++end_;
-        ++contents_size_;
-        if (end_ == array_size_) {
-            end_ = 0;
-        }
-    }
 };
+
+#endif //CIRCULAR_BUFFER_H
